42fdbf636ee73bf00671988c8366f950cdc9c1ca
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-02-17 19:15:22 +0530

    Add event listeners

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,1 +1,6 @@
-alert("Link JS to HTML");
+const nodeList = document.querySelectorAll(".drum");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].addEventListener("click", function () {
+    alert("Oii");
+  });
+}
