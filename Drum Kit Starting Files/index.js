191f2cb0e7b92153cb93de3908fd8f77240e8d79
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-02-17 19:25:12 +0530

    Add single drum sound

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,13 @@
 const nodeList = document.querySelectorAll(".drum");
+
+var audio = new Audio("./sounds/tom-1.mp3");
+
 for (let i = 0; i < nodeList.length; i++) {
+
   nodeList[i].addEventListener("click", function () {
-    alert("Oii");
+
+    audio.play();
+
   });
+
 }
