27fe448e99d0c1d474cb0ff206d080b2a11c0d99
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-02-18 18:37:23 +0530

    Add solution to challenge (FINAL)

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,7 +4,15 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("h1").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColor = buttonColors[randomNumber];
@@ -15,14 +23,69 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var randomChosenColorSound = new Audio(
-    "./sounds/" + randomChosenColor + ".mp3"
-  );
-  randomChosenColorSound.play();
+  playSound(randomChosenColor);
 }
 
 $(".btn").on("click", function () {
   var userChosenColor = $(this).attr("id");
 
   userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
+
+function playSound(name) {
+  var sound = new Audio("./sounds/" + name + ".mp3");
+  sound.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keydown(function () {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+
+    var wrong = new Audio("./sounds/wrong.mp3");
+    wrong.play();
+
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  started = false;
+
+  gamePattern = [];
+}
