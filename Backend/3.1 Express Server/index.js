88b57b3d0b6736461696b725625c7fa84d433fa2
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-03-16 20:04:52 +0530

    Create express server

---
+++ Backend/3.1 Express Server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {   // call back ---->  () => {}
+
+  console.log(`Server running on port ${port}.`);
+});
