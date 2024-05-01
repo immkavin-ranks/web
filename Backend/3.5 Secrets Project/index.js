43260e173b417173876960d85f172ed8b36d8770
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-01 15:21:32 +0530

    Implement secrets project

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,39 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var page;
+const check = (req, res, next) => {
+  if (req.body["password"] === "ILoveProgramming") {
+    page = "secret";
+  } else {
+    page = "index";
+  }
+  next();
+};
+
+app.use(check);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  res.sendFile(__dirname + `/public/${page}.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Application running on port ${PORT}`);
+});
