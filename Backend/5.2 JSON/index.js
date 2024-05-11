8d97b7fdfc6f0f95410cbeaf7df3db8b460e191f
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-11 11:48:51 +0530

    Remove index variable

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -10,7 +10,7 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let index, recipe;
+let recipe;
 const recipes = JSON.parse(recipeJSON);
 
 function findIndex(req, res, next) {
