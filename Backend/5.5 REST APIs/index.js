dd90fee77819c411e053d2668da70f602285fa42
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-14 11:12:55 +0530

    Use req.body instead of manually sending objects

--- Backend/5.5 REST APIs/index.js
+++ Backend/5.5 REST APIs/index.js
@@ -38,12 +38,8 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  const body = {
-    secret: req.body.secret,
-    score: req.body.score,
-  };
   try {
-    const result = await axios.post(`${API_URL}/secrets`, body, config);
+    const result = await axios.post(`${API_URL}/secrets`, req.body, config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     console.error(error.message);
@@ -56,10 +52,7 @@ app.post("/put-secret", async (req, res) => {
     // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
     const result = await axios.put(
       `${API_URL}/secrets/${searchId}`,
-      {
-        secret: req.body.secret,
-        score: req.body.score,
-      },
+      req.body,
       config
     );
     res.render("index.ejs", { content: JSON.stringify(result.data) });
@@ -75,10 +68,7 @@ app.post("/patch-secret", async (req, res) => {
 
     const result = await axios.patch(
       `${API_URL}/secrets/${searchId}`,
-      {
-        secret: req.body.secret,
-        score: req.body.score,
-      },
+      req.body,
       config
     );
     res.render("index.ejs", { content: JSON.stringify(result.data) });
