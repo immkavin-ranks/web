52d9e1105731cf2acb6bcda307da9a06dc7092ef
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-19 19:47:57 +0530

    Add my solution to blog API

--- Backend/6.1+Blog+API+Project/index.js
+++ Backend/6.1+Blog+API+Project/index.js
@@ -41,14 +41,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+  res.json(post);
+});
 
 //CHALLENGE 3: POST a new post
+app.post("/posts", (req, res) => {
+  const newPost = {
+    id: posts.splice(-1)[0].id + 1,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  posts.push(newPost);
+  res.json(newPost);
+});
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+  const postIndex = posts.findIndex((post) => post.id === postId);
+  const patchPost = {
+    id: postId,
+    title: req.body.title || post.title,
+    content: req.body.content || post.content,
+    author: req.body.author || post.author,
+    date: new Date(),
+  };
+  posts[postIndex] = patchPost;
+  res.json(patchPost);
+});
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((post) => post.id === postId);
+  if (postIndex > -1) {
+    posts.splice(postIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res.status(404).json({ error: `Post with ID ${postId} is not found.` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
