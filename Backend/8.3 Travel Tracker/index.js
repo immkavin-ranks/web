3f429037ffa4ce21093d664460f5317f57d60843
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-26 11:49:33 +0530

    Solve travel tracker challenge 1

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -1,14 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  password: "pg",
+  host: "localhost",
+  database: "world",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
+
+db.connect();
+
+let countries = [];
+db.query("SELECT country_code FROM visited_countries", (err, res) => {
+  if (err) {
+    console.error(err.stack);
+  } else {
+    res.rows.forEach((country) => {
+      countries.push(country.country_code);
+    });
+  }
+  db.end();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  console.log(countries);
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
 app.listen(port, () => {
