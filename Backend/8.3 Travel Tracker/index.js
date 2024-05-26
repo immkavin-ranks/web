98d574e0f4db704b48388133f667f236207dc601
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-26 15:51:26 +0530

    Implement error handlers

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -18,14 +18,56 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-  let countries = []
-  result.rows.forEach(country => {
-    countries.push(country.country_code)
-  })
-  console.log(countries)
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  console.log(countries);
   res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const country_name = req.body.country;
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || LOWER($1) || '%'",
+      [country_name]
+    );
+    if (result.rows.length !== 0) {
+      try {
+        await db.query(
+          "INSERT INTO visited_countries (country_code) VALUES ($1)",
+          [result.rows[0].country_code]
+        );
+        res.redirect("/");
+      } catch (err) {
+        const countries = await checkVisisted();
+        res.render("index.ejs", {
+          error: "Country already visited",
+          total: countries.length,
+          countries: countries,
+        });
+      }
+    } else {
+      throw "Country not found";
+    }
+  } catch (err) {
+    const countries = await checkVisisted();
+
+    res.render("index.ejs", {
+      error: err,
+      total: countries.length,
+      countries: countries,
+    });
+  }
 });
 
 app.listen(port, () => {
