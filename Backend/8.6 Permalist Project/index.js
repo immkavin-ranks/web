ee1cd47ca1f945085432999f975fbcaaf0b5cdca
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-07 19:04:46 +0530

    Add solution

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -1,33 +1,61 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "pg",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
-
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const response = await db.query("SELECT *  FROM items");
+  const items = response.rows;
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+    res.redirect("/?error=Failed+to+add+item");
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const id = req.body.updatedItemId;
+  const title = req.body.updatedItemTitle;
+  try {
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [title, id]);
+  } catch (error) {
+    console.error(error.message);
+  }
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
-
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+  } catch (error) {
+    console.error(error.message);
+  }
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
