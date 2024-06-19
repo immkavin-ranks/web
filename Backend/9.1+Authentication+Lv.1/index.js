fd43458b322192a551e36a5b789132f45800f9b7
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-19 20:59:41 +0530

    my solution to auth level 1

--- Backend/9.1+Authentication+Lv.1/index.js
+++ Backend/9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  database: "secrets",
+  host: "localhost",
+  port: 5432,
+  password: "pg",
+});
 
 const app = express();
 const port = 3000;
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,40 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
 
-app.post("/login", async (req, res) => {});
+  try {
+    await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [
+      username,
+      password,
+    ]);
+    console.log("registered");
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const response = await db.query(
+      "select * from users where email = $1 and password = $2",
+      [username, password]
+    );
+    const user = response.rows[0];
+    if (user) {
+      console.log("login success" + user);
+    } else {
+      console.log("please register");
+    }
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
