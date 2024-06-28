24c59ed34da9ed120b6c057c99154713aa901155
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-28 22:34:35 +0530

    jsx code challenge

---
+++ React.js/jsx-code-challenge/src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Hello</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
