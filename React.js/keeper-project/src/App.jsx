b90f9c8566b9b90513d40962670099a219f64dc9
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-08-24 22:08:30 +0530

    Add note

--- React.js/keeper-project/src/App.jsx
+++ React.js/keeper-project/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <Header />
       <Note />
+      <Note />
       <Footer />
     </>
   );
--- React.js/keeper-project/src/Note.jsx
+++ React.js/keeper-project/src/Note.jsx
@@ -1,6 +1,7 @@
+import "./styles/Note.css";
 export default function Note() {
   return (
-    <div>
+    <div className="noteCard">
       <h2>This is the note title</h2>
       <p>This is the note content</p>
     </div>
