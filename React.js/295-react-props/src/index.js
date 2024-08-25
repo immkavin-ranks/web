2ab2bc37919739ba134e0bb4d0730f8edb2fca18
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-08-25 11:10:49 +0530

    295-react-props

---
+++ React.js/295-react-props/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card
+      name="Kavin"
+      img="https://avatars.githubusercontent.com/u/126098744"
+      tel="+91 79758 10056"
+      email="kavinsde@gmail.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
