28415ad92cddfa18053bf1cb189c3492a0dc7978
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-12-13 09:07:11 +0530

    managing component tree

---
+++ React.js/318-managing-components-tree-practice/src/components/InputArea.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function InputArea({ addItem }) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          addItem(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
